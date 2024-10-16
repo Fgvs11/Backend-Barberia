509491c4f1c661eb46e0f751963fad67167fb146
Fgvs <fgvizcainos@gmail.com>
2024-10-16 07:47:32 -0600

    Agregue el inicio del Django y ya coloque los modelos de la base de datos

---
+++ barbershop/appointments/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+# Create your models here.
+class Cliente(models.Model):
+    id_cliente = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 40)
+    apellido_paterno = models.CharField(max_length = 30)
+    apellido_materno = models.CharField(max_length = 30)
+    telefono = models.CharField(max_length = 10)
+    fecha_registro = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellido_paterno + ' ' + self.apellido_materno
+    
+class Barberos(models.Model):
+    id_barbero = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 40)
+    apellido_paterno = models.CharField(max_length = 30)
+    apellido_materno = models.CharField(max_length = 30)
+    telefono = models.CharField(max_length = 10)
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellido_paterno + ' ' + self.apellido_materno
+    
+class Servicios(models.Model):
+    id_servicio = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 100)
+    precio = models.DecimalField(max_digits = 5, decimal_places = 2)
+    tiempo_aproximado = models.FloatField()
+
+    def __str__(self):
+        return self.nombre
+    
+class EstadoCitas(models.Model):
+    id_estado = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.nombre
+    
+class Citas(models.Model):
+    id_cita = models.AutoField(primary_key = True)
+    id_cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE)
+    id_barbero = models.ForeignKey(Barberos, on_delete = models.CASCADE)
+    id_servicio = models.ForeignKey(Servicios, on_delete = models.CASCADE)
+    fecha_inicio= models.DateTimeField()
+    fecha_finalizacion= models.DateTimeField()
+    id_estado = models.ForeignKey(EstadoCitas, on_delete = models.CASCADE)
+    token = models.BooleanField(default = True)
+
+    def __str__(self):
+        return f"{self.cliente.nombre} {self.cliente.apellido_paterno} {self.cliente.apellido_materno} - {self.fecha_inicio} to {self.fecha_finalizacion}"
