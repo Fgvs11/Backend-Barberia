0e244502fdd251c75e08da19fbfbb8fabe2ced25
Fgvs <fgvizcainos@gmail.com>
2024-10-24 16:15:37 -0600

    Hot fix

--- barbershop/barbershop/settings.py
+++ barbershop/barbershop/settings.py
@@ -77,6 +77,8 @@ REST_FRAMEWORK = {
 # ]
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_CREDENTIALS = True
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
