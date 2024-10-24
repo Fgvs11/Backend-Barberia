188cbc67a831e44ddd45fb5e0964c87f447052a6
Fgvs <fgvizcainos@gmail.com>
2024-10-24 17:15:09 -0600

    Hot fix views y urls

--- barbershop/barbershop/settings.py
+++ barbershop/barbershop/settings.py
@@ -80,7 +80,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
